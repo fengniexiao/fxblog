f316bd2a330fa85637375e78398128ca71a1f9ca
fengniexiao <1249783634@qq.com>
2020-06-27 20:00:42 +0800

    Site updated: 2020-06-27 20:00:41

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ self.addEventListener('active', function () {
 });
 
 var precacheUrls = [
-'/','/posts/13522/','/posts/16107/',
+'/fxblog/','/fxblog/posts/13522/','/fxblog/posts/16107/',
 ];
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
